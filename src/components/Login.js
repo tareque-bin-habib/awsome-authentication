84bd7ae21edd915ad3924a7f52ba3e0158a06e76
tareque-bin-habib <shad.tbh@gmail.com>
2022-11-25 22:46:34 +0600

    register form added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Login = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -10,18 +18,18 @@ const Login = () => {
                         <p className="py-6">Provident cupiditate voluptatem et in.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleSubmit} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
