65ba9c9c8179783d0c3d6c94abb4beb236f586cc
tareque-bin-habib <shad.tbh@gmail.com>
2022-11-26 00:23:58 +0600

    context api added

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react';
+
+const AuthContext = createContext();
+
+const UserContext = ({ children }) => {
+    const user = { displayName: 'Akash' }
+    const authInfo = { user: user }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
