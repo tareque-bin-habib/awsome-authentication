30f9f004f8f9c42a126365ba8dd4d49d7f2c029a
tareque-bin-habib <shad.tbh@gmail.com>
2022-11-25 22:15:24 +0600

    form added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAjKvmsHYJO4BXWiM2bahlWt0nxSt2k0ZM",
+    authDomain: "auth-router-context-eec77.firebaseapp.com",
+    projectId: "auth-router-context-eec77",
+    storageBucket: "auth-router-context-eec77.appspot.com",
+    messagingSenderId: "668034943575",
+    appId: "1:668034943575:web:47955fd392fb7f4b81f112"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
